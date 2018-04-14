6ece8e8d0d1fb5e218a9e056bb3e850c8bb6d6a8
selech <selech07@gmail.com>
2018-04-14 17:08:11 +0200

    Sorry 'bout that..

---
+++ Assets/BuildingBlockScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingBlockScript : MonoBehaviour
+{
+    public Color Color;
+    public List<MeshRenderer> MeshesToColor;
+
+	// Use this for initialization
+	void Start () {
+        var material = new Material(Shader.Find("Standard"));
+
+        foreach (var mesh in MeshesToColor)
+        {
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
