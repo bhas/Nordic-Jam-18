ebfa8b48db581d993885d985084bf5193edf1e3e
selech <selech07@gmail.com>
2018-04-14 20:47:27 +0200

    Fixed all building pieces

--- Assets/BuildingBlockScript.cs
+++ Assets/BuildingBlockScript.cs
@@ -7,17 +7,25 @@ public class BuildingBlockScript : MonoBehaviour
 {
     public Color PrimaryColor;
     public Color SecondaryColor;
+    public Color RoofColor;
     public List<MeshRenderer> MeshesToColor;
 
 	// Use this for initialization
 	void Update () {
         var primaryMaterial = new Material(Shader.Find("Standard"));
         var secondaryMaterial = new Material(Shader.Find("Standard"));
+        var roofMaterial = new Material(Shader.Find("Standard"));
+
         primaryMaterial.color = PrimaryColor;
         secondaryMaterial.color = SecondaryColor;
+        roofMaterial.color = RoofColor;
+
         foreach (var mesh in MeshesToColor)
         {
-            mesh.materials = new Material[] {primaryMaterial, secondaryMaterial };
+            if(mesh.materials.Length == 3)
+                mesh.materials = new Material[] { roofMaterial, mesh.materials[1], mesh.materials[2] };
+            else
+                mesh.materials = new Material[] { primaryMaterial, secondaryMaterial};
         }
 	}
 }
