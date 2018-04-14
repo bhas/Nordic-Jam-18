fe7fd53f842715de33fde2b3899eb989eb72b04c
selech <selech07@gmail.com>
2018-04-14 19:45:28 +0200

    Auto stash before merge of "master" and "origin/master"

--- Assets/AnchorPointAligner.cs
+++ Assets/AnchorPointAligner.cs
@@ -4,10 +4,12 @@ public class AnchorPointAligner : MonoBehaviour
 {
     public Transform AnchorLocator;
 
+    public float Distance;
+
     // Update is called once per frame
     void FixedUpdate()
     {
-        this.gameObject.transform.position = AnchorLocator.position;
+        this.gameObject.transform.position = AnchorLocator.position - new Vector3(0, Distance, 0);
         this.gameObject.transform.rotation = Quaternion.Euler(new Vector3(0, AnchorLocator.rotation.eulerAngles.y, 0));
     }
 }
