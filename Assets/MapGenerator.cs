f8a3c9a2baab24af262870685d78446c862e488a
selech <selech07@gmail.com>
2018-04-18 21:49:28 +0200

    Initial mapgenerator stuff and though

---
+++ Assets/MapGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public int MapWidth;
+    public int MapDepth;
+
+    public List<GameObject> RoadPieces;
+
+    public int Steps;
+
+    // Use this for initialization
+    void Start()
+    {
+        var currentPosition = new Vector3();
+        var currentRotation = 0f;
+        for (int i = 0; i < Steps; i++)
+        {
+            var currentRoadPiece = RoadPieces[UnityEngine.Random.Range(0, RoadPieces.Count - 1)];
+            var go = Instantiate(currentRoadPiece);
+            go.transform.position = currentPosition;
+            RoadPiece rp = go.GetComponent<RoadPiece>();
+            go.transform.Rotate(new Vector3(0,currentRotation,0));
+
+            var vector = Quaternion.Euler(0, currentRotation, 0) * (new Vector3(rp.Direction.x, 0, rp.Direction.y) * 4);
+            currentPosition += vector;
+            currentRotation += rp.Rotation;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/RoadPiece.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadPiece : MonoBehaviour {
+
+    public GameObject Piece;
+    public Vector2 Direction;
+    public float Rotation;
+    public bool Split;
+    public Vector2 SplitDirection;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
